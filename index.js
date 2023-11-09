17b0038585d4e19e4cdc31781b458daaa71cbfc5
evansurtel <evan.j.surtel@gmail.com>
2023-11-08 22:38:06 -0500

    setting up discord api

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(
+	'MTE3MTkwMDMzNzI0Mjc4Nzk1Mw.GEKXPw.JazMM5IHt-YEOffy_0vNRWrItxScc2PsAd_MCA'
+);
